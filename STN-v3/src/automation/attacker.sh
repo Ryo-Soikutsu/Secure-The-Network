1ce64ce5a09df3962581bab3da7e8225b9f439a6
Ryo-Soikutsu <64062496+Ryo-Soikutsu@users.noreply.github.com>
2026-08-03 09:16:41 +0800

    yes

--- STN-v3/src/automation/attacker.sh
+++ STN-v3/src/automation/attacker.sh
@@ -2,3 +2,10 @@
 
 # Script only for attacker setup
 # To run the actual attack simulation, run the downloaded Metasploit module
+
+set -e
+
+sudo apt update -y
+sudo apt upgrade -y
+
+wget https://raw.githubusercontent.com/Ryo-Soikutsu/Secure-The-Network/refs/heads/main/STN-v3/src/automation/stn-attacker.rb
